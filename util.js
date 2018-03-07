8b0ec0805354072d0007897dbc6111f3ecba2c2e
dongxiao.lu <dongxiao.lu@qunar.com>
2018-03-07 16:04:05 +0800

    add number

--- util.js
+++ util.js
@@ -24,73 +24,104 @@ var util = {
 	    }
 	},
 
-	// 带小数的加法运算
-	add: function (arg1, arg2) {
-        var r1, r2, m;
-        try {
-            r1 = arg1.toString().split(".")[1].length;
-        } catch (e) {
-            r1 = 0;
-        }
-        try {
-            r2 = arg2.toString().split(".")[1].length;
-        } catch (e) {
-            r2 = 0;
-        }
-        m = Math.pow(10, Math.max(r1, r2));
-        return (arg1 * m + arg2 * m) / m;
-    },
+    // 运算相关方法
+    operation: {
+        // 带小数的加法运算
+        add: function (arg1, arg2) {
+            var r1, r2, m;
+            try {
+                r1 = arg1.toString().split(".")[1].length;
+            } catch (e) {
+                r1 = 0;
+            }
+            try {
+                r2 = arg2.toString().split(".")[1].length;
+            } catch (e) {
+                r2 = 0;
+            }
+            m = Math.pow(10, Math.max(r1, r2));
+            return (arg1 * m + arg2 * m) / m;
+        },
 
-    // 带小数的减法运算
-    sub : function( num1, num2 ) {
-        var baseNum, baseNum1, baseNum2;
-        var precision;// 精度
-        try {
-            baseNum1 = num1.toString().split(".")[1].length;
-        } catch (e) {
-            baseNum1 = 0;
-        }
-        try {
-            baseNum2 = num2.toString().split(".")[1].length;
-        } catch (e) {
-            baseNum2 = 0;
-        }
-        baseNum = Math.pow(10, Math.max(baseNum1, baseNum2));
-        precision = (baseNum1 >= baseNum2) ? baseNum1 : baseNum2;
-        return ((num1 * baseNum - num2 * baseNum) / baseNum).toFixed(precision);
-    },
+        // 带小数的减法运算
+        sub : function( num1, num2 ) {
+            var baseNum, baseNum1, baseNum2;
+            var precision;// 精度
+            try {
+                baseNum1 = num1.toString().split(".")[1].length;
+            } catch (e) {
+                baseNum1 = 0;
+            }
+            try {
+                baseNum2 = num2.toString().split(".")[1].length;
+            } catch (e) {
+                baseNum2 = 0;
+            }
+            baseNum = Math.pow(10, Math.max(baseNum1, baseNum2));
+            precision = (baseNum1 >= baseNum2) ? baseNum1 : baseNum2;
+            return ((num1 * baseNum - num2 * baseNum) / baseNum).toFixed(precision);
+        },
 
-    // 带小数的除法运算
-    divi : function (arg1, arg2) {
-        var t1 = 0, t2 = 0, r1, r2;
-        try {
-            t1 = arg1.toString().split(".")[1].length
-        } catch (e) {
-        }
-        try {
-            t2 = arg2.toString().split(".")[1].length
-        } catch (e) {
-        }
-        with (Math) {
-            r1 = Number(arg1.toString().replace(".", ""))
-            r2 = Number(arg2.toString().replace(".", ""))
+        // 带小数的除法运算
+        divi : function (arg1, arg2) {
+            var t1 = 0, t2 = 0, r1, r2;
+            try {
+                t1 = arg1.toString().split(".")[1].length;
+            } catch (e) {
+            }
+            try {
+                t2 = arg2.toString().split(".")[1].length;
+            } catch (e) {
+            }
+            r1 = Number(arg1.toString().replace(".", ""));
+            r2 = Number(arg2.toString().replace(".", ""));
             return (r1 / r2) * pow(10, t2 - t1);
+        },
+        
+        // 带小数的乘法运算
+        multi : function (arg1, arg2) {
+            var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
+            try {
+                m += s1.split(".")[1].length;
+            } catch (e) {
+            }
+            try {
+                m += s2.split(".")[1].length;
+            } catch (e) {
+            }
+            return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
         }
     },
-    
-    // 带小数的乘法运算
-    multi : function (arg1, arg2) {
-        var m = 0, s1 = arg1.toString(), s2 = arg2.toString();
-        try {
-            m += s1.split(".")[1].length
-        } catch (e) {
+
+    // 数字格式化
+    number: function(number, decimals, dec_point, thousands_sep) {
+        //number	必需，要格式化的数字
+        //decimals	可选，规定多少个小数位。
+        //dec_point	可选，规定用作小数点的字符串（默认为 . ）。
+        //thousands_sep	可选，规定用作千位分隔符的字符串（默认为','）
+        number = (number + "").replace(/[^0-9+\-Ee.]/g, "");
+        var n = !isFinite(+number) ? 0 : +number,
+            prec = !isFinite(+decimals) ? 0 : Math.abs(decimals), // 取绝对值
+            sep = thousands_sep || ",",
+            dec = dec_point || ".",
+            s = "",
+            toFixedFix = function(n, prec) {
+                var k = Math.pow(10, prec);
+                return "" + Math.round(n * k) / k;
+            };
+        s = (prec ? toFixedFix(n, prec) : "" + Math.round(n)).split('.');
+        // 整数部分每隔3位加入分隔符
+        if (s[0].length > 3) {
+            s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
         }
-        try {
-            m += s2.split(".")[1].length
-        } catch (e) {
+        // 小数部分如果不足decimals，则补0.
+        if ((s[1] || "").length < prec) {
+            s[1] = s[1] || "";
+            s[1] += new Array(prec - s[1].length + 1).join("0");
         }
-        return Number(s1.replace(".", "")) * Number(s2.replace(".", "")) / Math.pow(10, m);
+        return s.join(dec);
     }
+	
 };
 
 module.exports = util;
